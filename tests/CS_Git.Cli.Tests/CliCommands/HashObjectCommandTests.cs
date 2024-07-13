85efe1b89b23fd6a5112e59ae23e759de5765866
vince <vincepr@gmx.de>
2024-07-13 17:09:50 +0200

    up

--- tests/CS_Git.Cli.Tests/CliCommands/HashObjectCommandTests.cs
+++ tests/CS_Git.Cli.Tests/CliCommands/HashObjectCommandTests.cs
@@ -19,12 +19,9 @@ public class HashObjectCommandTests
         Console.SetOut(_stringWriter);
     }
 
-    // auto crlf might fuck these tests up. As some of these test files might have 'other' line-endings.
-    // it is disabled on purpose on the whole repository. Also because CsGit will not support any auto-crlf before hashing.
-    // remark for sanity reasons we just rewrite the tests for 'fixed' crlf files
     [TestCase("hashtestbinaryfile", "8c48471bdb782797a91d8fe3955a70131abc5150")]
-    [TestCase("hashtestfile", "ada51d4bcc6374b76b0041aa316409938ff900c7")]
-    [TestCase("README.md", "96585df1178e25ace823205e9a3730899fce4812")]
+    [TestCase("hashtestfile", "f10745c515e5f1d2739d9ce3856e492e0729828e")]
+    [TestCase("README.md", "d99b9f43089191d561943ba3fa10541c6fc769c2")]
     public async Task Textfile_GitHashed_Equals_CsgitHashed(string file, string expectedHash)
     {
         // Act
@@ -51,6 +48,8 @@ public class HashObjectCommandTests
         Assert.That(expectedHash, Is.EqualTo(output));
     }
 
+    // auto crlf might fuck these tests up. As some of these test files might have 'other' line-endings.
+    // it is disabled on purpose on the whole repository. Also because CsGit will not support any auto-crlf before hashing.
     private string CaptureGitHashObject(string path)
     {
         var proc = new Process 
